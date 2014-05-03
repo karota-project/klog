da5bc12c4b439dc14bdc3428bd57fb04c1e09a59
TatsuyaEgawa <tatsuya@TatsuyaEgawa-no-MacBook-Air.local>
2014-05-03 13:53:59 +0900

    testコードを追加

--- klog_test.go
+++ klog_test.go
@@ -1,10 +1,24 @@
-package main
+package klog
 
 import (
   "testing"
   "./klog"
 )
 
-func main () {
+func TestPrintlog(t *testing.T) {
+	/* expected TRUE test */
+    actual := klog.Printlog("test")
+    expected := true
+    if actual != expected {
+        t.Errorf("got %v\nwant %v", actual, expected)
+    }
+}
 
+func TestPrintfile(t *testing.T) {
+    /* expected TRUE test */
+    actual := klog.Printfile("test", "test")
+    expected := true
+    if actual != expected {
+        t.Errorf("got %v\nwant %v", actual, expected)
+    }
 }
