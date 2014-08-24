8c34413eefb651290fb4740c6b2ae007f6940dfa
egawa <tatsuya@TatsuyaEgawa-no-MacBook-Air.local>
2014-08-24 21:39:38 +0900

    fix test code

--- klog_test.go
+++ klog_test.go
@@ -5,19 +5,28 @@ import (
 	"testing"
 )
 
-func TestPrintlog(t *testing.T) {
+func TestStdout(t *testing.T) {
 	/* expected TRUE test */
-	actual := klog.Printlog("TestPrintfile")
-	expected := true
+	actual := klog.Stdout("main")
+	var expected error = nil
 	if actual != expected {
 		t.Errorf("got %v,want %v", actual, expected)
 	}
 }
 
-func TestPrintfile(t *testing.T) {
+func TestWriteFile(t *testing.T) {
 	/* expected TRUE test */
-	actual := klog.Printfile("TestPrintfile", "test")
-	expected := true
+	actual := klog.WriteFile("main", "sample.log")
+	var expected error = nil
+	if actual != expected {
+		t.Errorf("got %v,want %v", actual, expected)
+	}
+}
+
+func TestSyslog(t *testing.T) {
+	/* expected TRUE test */
+	actual := klog.Syslog(klog.LOG_NOTICE, "main")
+	var expected error = nil
 	if actual != expected {
 		t.Errorf("got %v,want %v", actual, expected)
 	}
